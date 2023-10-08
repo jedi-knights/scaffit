de33a17c80ca05b4f3a0f88c24d0fa562da1764d
Omar Crosby <omar.crosby@gmail.com>
2023-10-08 02:39:16 -0400

    chore(docs): remove scaffold.jpeg (#8)

--- .releaserc.js
+++ .releaserc.js
@@ -12,29 +12,29 @@ module.exports = {
     verifyConditions: [
         '@semantic-release/changelog',
         '@semantic-release/git',
-        '@semantic-release/github',
+        '@semantic-release/github'
     ],
 
     // Responsible for determining the type of release to make (major, minor or patch)
     // See: https://github.com/semantic-release/commit-analyzer#configuration
     analyzeCommits: [
-        '@semantic-release/commit-analyzer',
+        '@semantic-release/commit-analyzer'
     ],
 
     // Responsible for generating the content of the release note
     generateNotes: [
-        '@semantic-release/release-notes-generator',
+        '@semantic-release/release-notes-generator'
     ],
 
     // Responsible for preparing the release, for example creating or updating files such as package.json, CHANGELOG.md, documentation or compiled assets
     prepare: [
         '@semantic-release/changelog',
-        '@semantic-release/git',
+        '@semantic-release/git'
     ],
 
     // Responsible for preparing the release, for example creating or updating files such as package.json, CHANGELOG.md, documentation or compiled assets
     publish: [
-        '@semantic-release/github',
+        '@semantic-release/github'
     ],
 
     plugins: [
@@ -44,14 +44,14 @@ module.exports = {
         [
             '@semantic-release/exec',
             {
-                prepareCmd: 'echo ${nextRelease.version} > VERSION',
-            },
+                prepareCmd: 'echo ${nextRelease.version} > VERSION'
+            }
         ],
         '@semantic-release/npm',
         ['@semantic-release/git', {
             assets: ['./public/**/*', 'package.json', 'package-lock.json', 'CHANGELOG.md', 'VERSION'],
-            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
         }],
-        '@semantic-release/github',
-    ],
-};
+        '@semantic-release/github'
+    ]
+}
