be02d96d42435124ba1f3de93b2b90a00697a31b
Omar Crosby <omar.crosby@gmail.com>
2023-10-07 20:48:12 -0400

    feat(build): adding semantic release

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,42 @@
 module.exports = {
     branches: ['main'],
+    ci: true,
+    debug: true,
+    dryRun: false,
+    tagFormat: '${version}',
+
+    // Global plugin options (will be passed to all plugins)
+    preset: 'conventionalcommits',
+
+    // Reponsible for verifying conditions necessary to proceed with the release
+    verifyConditions: [
+        '@semantic-release/changelog',
+        '@semantic-release/git',
+        '@semantic-release/github',
+    ],
+
+    // Responsible for determining the type of release to make (major, minor or patch)
+    // See: https://github.com/semantic-release/commit-analyzer#configuration
+    analyzeCommits: [
+        '@semantic-release/commit-analyzer',
+    ],
+
+    // Responsible for generating the content of the release note
+    generateNotes: [
+        '@semantic-release/release-notes-generator',
+    ],
+
+    // Responsible for preparing the release, for example creating or updating files such as package.json, CHANGELOG.md, documentation or compiled assets
+    prepare: [
+        '@semantic-release/changelog',
+        '@semantic-release/git',
+    ],
+
+    // Responsible for preparing the release, for example creating or updating files such as package.json, CHANGELOG.md, documentation or compiled assets
+    publish: [
+        '@semantic-release/github',
+    ],
+
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
@@ -11,6 +48,10 @@ module.exports = {
             },
         ],
         '@semantic-release/npm',
+        ['@semantic-release/git', {
+            assets: ['./public/**/*', 'package.json', 'package-lock.json', 'CHANGELOG.md', 'VERSION'],
+            message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        }],
         '@semantic-release/github',
     ],
 };
